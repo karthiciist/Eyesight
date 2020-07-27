9c858043623f26db46dee92934ae03e50abdab9a
Karthic Srinivasan <karthiciist@gmail.com>
2020-07-27 14:07:45 +0530

    Initial commit

--- jsontest.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_string = '{"1": {"xaxis": "1","yaxis": "2","width": "3","height": "4","rotate": "5","scalex": "6","scaley": "7","lable": "8","text": "9"}, "2": {"xaxis": "1","yaxis": "2","width": "3","height": "4","rotate": "5","scalex": "6","scaley": "7","lable": "8","text": "9"}, "3": {"xaxis": "1","yaxis": "2","width": "3","height": "4","rotate": "5","scalex": "6","scaley": "7","lable": "8","text": "9"}}'
-json_dictionary = json.loads(json_string)
-for key in json_dictionary:
-    indi = json_dictionary[key]
-    for key1 in indi:
-        print(key1, ":", indi[key1])
-    print("--------------------")
--- tabletest.py
+++
@@ -1,3 +0,0 @@
-from invoice2data import extract_data
-
-result = extract_data('C:/Users/ET437GL/Desktop/table4.png')
